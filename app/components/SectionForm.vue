c1c4dfd527e8276dc7661f2f1b7c68c45512c361
Morozyuk Daniil <morozyuk.d.p@gmail.com>
2026-03-15 12:21:26 +0300

    Added Google Form

--- app/components/SectionForm.vue
+++ app/components/SectionForm.vue
@@ -14,40 +14,44 @@
           </p>
         </div>
         <div class="col-md-10 mx-auto col-lg-5">
-          <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
-            <div class="form-floating mb-3">
-              <input
-                type="text"
-                class="form-control"
-                id="clientName"
-                placeholder="Иванов Иван Иванович"
-              />
-              <label for="clientName">Ваше имя</label>
-            </div>
-            <div class="form-floating mb-3">
-              <input
-                type="email"
-                class="form-control"
-                id="clientEmail"
-                placeholder="johndoe@example.com"
-              />
-              <label for="clientEmail">Ваша почта</label>
-            </div>
-            <div class="form-floating mb-3">
-              <textarea
-                name="task"
-                id="clientTask"
-                cols="30"
-                rows="10"
-                class="form-control"
-                style="height: 150px"
-              ></textarea>
-              <label for="clientTask"> Опишите свою задачу... </label>
-            </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit">
-              Записаться на консультацию
-            </button>
-          </form>
+          <!--          <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">-->
+          <!--            <div class="form-floating mb-3">-->
+          <!--              <input-->
+          <!--                type="text"-->
+          <!--                class="form-control"-->
+          <!--                id="clientName"-->
+          <!--                placeholder="Иванов Иван Иванович"-->
+          <!--              />-->
+          <!--              <label for="clientName">Ваше имя</label>-->
+          <!--            </div>-->
+          <!--            <div class="form-floating mb-3">-->
+          <!--              <input-->
+          <!--                type="email"-->
+          <!--                class="form-control"-->
+          <!--                id="clientEmail"-->
+          <!--                placeholder="johndoe@example.com"-->
+          <!--              />-->
+          <!--              <label for="clientEmail">Ваша почта</label>-->
+          <!--            </div>-->
+          <!--            <div class="form-floating mb-3">-->
+          <!--              <textarea-->
+          <!--                name="task"-->
+          <!--                id="clientTask"-->
+          <!--                cols="30"-->
+          <!--                rows="10"-->
+          <!--                class="form-control"-->
+          <!--                style="height: 150px"-->
+          <!--              ></textarea>-->
+          <!--              <label for="clientTask"> Опишите свою задачу... </label>-->
+          <!--            </div>-->
+          <!--            <button class="w-100 btn btn-lg btn-primary" type="submit">-->
+          <!--              Записаться на консультацию-->
+          <!--            </button>-->
+          <!--          </form>-->
+          <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSfYqFjA-R1jDF4kn8Qj7GwmrKUZRpjK1nJWBIOtpCFelVgpXA/viewform?embedded=true"
+              width="100%" height="500" frameborder="0" marginheight="0" marginwidth="0">Загрузка…
+          </iframe>
         </div>
       </div>
     </div>
